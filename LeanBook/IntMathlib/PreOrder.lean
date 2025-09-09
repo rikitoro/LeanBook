a17ed91e1ac56033bfb0e281766b4962bf3c3d76
rikitoro <>
2025-09-09 22:44:03 +0900

    chap08 update

--- LeanBook/IntMathlib/PreOrder.lean
+++ LeanBook/IntMathlib/PreOrder.lean
@@ -28,3 +28,27 @@ theorem MyInt.ofNat_zero_eq_zero : MyInt.ofMyNat 0 = (0 : MyInt) := by
 example : ((0 : MyNat) : MyInt) = (0 : MyInt) := by
   guard_target =ₛ ↑(0 : MyNat) = (0 : MyInt)
   simp
+
+@[norm_cast]
+theorem MyInt.ofMyNat_inj {m n : MyNat} :
+  (m : MyInt) = (n : MyInt) ↔ m = n := by
+  constructor <;> intro h
+  case mp =>
+    have : (m, 0) ≈ (n, 0) := by
+      apply Quotient.exact h
+    notation_simp at this
+    simp_all
+  case mpr =>
+    rw [h]
+
+@[simp]
+theorem MyInt.ofMyNat_eq_zero {n : MyNat} : (n : MyInt) = 0 ↔ n = 0 := by
+  constructor <;> intro h
+  . rw [show (0 : MyInt) = ↑(0 : MyNat) from rfl] at h
+    norm_cast at h
+  . simp_all
+
+@[push_cast]
+theorem MyInt.ofNat_add (m n : MyNat) :
+  ↑(m + n) = (m : MyInt) + (n : MyInt) := by
+  rfl
