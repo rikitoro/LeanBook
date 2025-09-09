fdeeb19f2259b49f725ea00b182925ee510e53dd
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2025-09-09 10:01:52 +0900

    update chap6

--- LeanBook/NatOrder/DecidableOrd.lean
+++ LeanBook/NatOrder/DecidableOrd.lean
@@ -50,3 +50,53 @@ instance (a b : MyNat) : Decidable (a ≤ b) := by
 
 
 #check MyNat.ble.induct
+/-
+MyNat.ble.induct (motive : MyNat → MyNat → Prop)
+  (case1 : ∀ (b : MyNat), motive MyNat.zero b)
+  (case2 : ∀ (n : MyNat), motive n.succ MyNat.zero)
+  (case3 : ∀ (a b : MyNat), motive a b → motive a.succ b.succ)
+  (a b : MyNat) : motive a b
+-/
+
+def MyNat.ble.inductAux (motive : MyNat → MyNat → Prop)
+  (case1 : ∀ (n : MyNat), motive 0 n)
+  (case2 : ∀ (n : MyNat), motive (n + 1) 0)
+  (case3 : ∀ (m n : MyNat), motive m n → motive (m + 1) (n + 1))
+  (m n : MyNat) : motive m n := by
+  induction m, n using MyNat.ble.induct with
+  | case1 n => apply case1
+  | case2 n => apply case2
+  | case3 m n h => apply case3; assumption
+
+theorem MyNat.le_impl (m n : MyNat) : MyNat.ble m n ↔ m ≤ n := by
+  induction m, n using ble.inductAux with
+  | case1 n =>
+    simp
+  | case2 n =>
+    simp_all
+  | case3 m n ih =>
+    simp [ih]
+
+instance : DecidableLE MyNat := fun n m =>
+  decidable_of_iff (MyNat.ble n m) (MyNat.le_impl n m)
+
+#eval 1 ≤ 3
+#eval 12 ≤ 13
+
+example : 1 ≤ 9 := by
+  decide
+
+example : 32 ≤ 47 := by
+  decide
+
+theorem MyNat.lt_impl (m n : MyNat) : ble (m + 1) n ↔ m < n := by
+  rw [le_impl]
+  rfl
+
+instance : DecidableLT MyNat := fun n m =>
+  decidable_of_iff (MyNat.ble (n + 1) m) (MyNat.lt_impl n m)
+
+example : 1 < 4 := by decide
+
+example : 23 < 32 ∧ 12 ≤ 24 := by
+  decide
