39ba631850d905e9e26388066081bf376df4374b
rikitoro <>
2025-09-11 21:00:37 +0900

    chap08 update

--- LeanBook/IntMathlib/DecidableOrd.lean
+++ LeanBook/IntMathlib/DecidableOrd.lean
@@ -45,3 +45,59 @@ theorem MyInt.ofMyNat_le (m n : MyNat) : (m : MyInt) ≤ (n : MyInt) ↔ m ≤ n
     exists k
     rw [← hk]
     norm_cast
+
+theorem MyInt.le_sub_iff (x y z : MyInt) : z ≤ x - y ↔ z + y ≤ x := by
+  simp
+
+theorem MyInt.sub_nonneg (m n : MyInt) : 0 ≤ m - n ↔ n ≤ m := by
+  simp
+
+theorem MyInt.bpos_iff (z : MyInt) : bpos z ↔ 0 ≤ z := by
+  induction z using Quotient.inductionOn with
+  | h a =>
+    obtain ⟨x, y⟩ := a
+    rw [bpos_def, mk_def]
+    rw [sub_nonneg]
+    norm_cast
+
+instance : DecidablePred (0 ≤ · : MyInt → Prop) := by
+  intro n
+  apply decidable_of_iff (MyInt.bpos n)
+  apply MyInt.bpos_iff
+
+example : 0 ≤ (3 : MyInt) := by
+  decide
+
+
+instance : DecidableLE MyInt := by
+  intro n m
+  apply decidable_of_iff (0 ≤ m - n)
+  apply MyInt.sub_nonneg
+
+#eval (3 : MyInt) ≤ 4
+
+example : (3 : MyInt) ≤ 4 := by
+  decide
+
+instance : DecidableLT MyInt := by
+  intro n m
+  dsimp [(· < ·), MyInt.lt]
+  infer_instance
+
+#eval (3 : MyInt) < 4
+
+example : (3 : MyInt) < 4 := by
+  decide
+
+instance : DecidableEq MyInt := decidableEqOfDecidableLE
+
+#eval (3 : MyNat) = 4
+
+example : ¬ (3 : MyInt) = 4 := by
+  decide
+
+example (a : MyInt) (h : ∃ k : MyNat, a = ↑k) :  0 ≤ a := by
+  obtain ⟨k, hk⟩ := h
+  notation_simp
+  use k
+  simp [hk]
---
+++ LeanBook/IntMathlib/LinearOrder.lean
@@ -0,0 +1,1 @@
+import LeanBook.IntMathlib.DecidableOrd
