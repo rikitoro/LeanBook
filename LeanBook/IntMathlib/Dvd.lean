09d1d27ddfe8ac52c883e048c9a6b258d6e71c1b
rikitoro <>
2025-09-15 19:01:51 +0900

    update

--- LeanBook/IntMathlib/Dvd.lean
+++ LeanBook/IntMathlib/Dvd.lean
@@ -11,7 +11,20 @@ theorem MyInt.dvd_iff_exists {m n : MyInt} : m ∣n ↔ ∃ k, n = m * k := by
 def MyInt.IsPrime (n : MyInt) : Prop :=
   n > 1 ∧ ∀ k, 1 < k → k < n → ¬ k ∣ n
 
-example {p a b : MyInt} (hprime : p.IsPrime) : p ∣ a * b → p ∣ a ∨ p ∣ b := by
+theorem MyInt.mul_prev_dvd {m n : MyInt} (a : MyInt) (h : m ∣ n) : a * m∣ a * n := by
+  obtain ⟨k, hk⟩ := h
+  use k
+  rw [hk]
+  ring
+
+
+  example {p a b : MyInt} (hprime : p.IsPrime) : p ∣ a * b → p ∣ a ∨ p ∣ b := by
   dsimp [MyInt.IsPrime] at hprime
   have ⟨h1, h2⟩ := hprime
-  sorry
+  intro h
+  by_cases hpa : p ∣ a
+  . left
+    assumption
+  . right
+    obtain ⟨k, hk⟩ := h
+    sorry
